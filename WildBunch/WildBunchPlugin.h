46dbca75de17ce2c5bc5b1b6a3e8810b225a6dff
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-09-06 12:09:52 -0700

    initial import

---
+++ WildBunch/WildBunchPlugin.h
@@ -0,0 +1,12 @@
+//
+//  WildBunchPlugin.h
+//  WildBunch
+//
+//  Created by Jean-Pierre Mouilleseaux on 6 Sept 2011.
+//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface WildBunchPlugIn : QCPlugIn
+@end
