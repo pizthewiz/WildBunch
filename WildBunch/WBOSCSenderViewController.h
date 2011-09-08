17cdf0226cb1133f46094ba60700c5490a35057c
Jean-Pierre Mouilleseaux <jean_pierre@mac.com>
2011-09-08 10:11:23 -0700

    add placeholder for custom view controller

---
+++ WildBunch/WBOSCSenderViewController.h
@@ -0,0 +1,12 @@
+//
+//  WBOSCSenderViewController.h
+//  WildBunch
+//
+//  Created by Jean-Pierre Mouilleseaux on 8 Sept 2011.
+//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface WBOSCSenderViewController : QCPlugInViewController
+@end
---
+++ WildBunch/WBOSCSenderViewController.m
@@ -0,0 +1,12 @@
+//
+//  WBOSCSenderViewController.m
+//  WildBunch
+//
+//  Created by Jean-Pierre Mouilleseaux on 8 Sept 2011.
+//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//
+
+#import "WBOSCSenderViewController.h"
+
+@implementation WBOSCSenderViewController
+@end
